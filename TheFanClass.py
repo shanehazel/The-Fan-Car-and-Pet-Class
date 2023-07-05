5e886b28bba6de0c581791bdc88c63f879f66354
Shane <gupitshanehazel@gmail.com>
2023-07-05 15:47:37 +0800

    pseudocode for fan class

---
+++ TheFanClass.py
@@ -0,0 +1,20 @@
+#pseudocode
+
+#class
+    #constant = value
+    #construct fan defaults
+    #get speed
+    #set speed
+    #get on
+    #set on
+    #get radius
+    #set radius
+    #get color
+    #set color
+
+#class test fan
+    #def test fan
+    #print output
+
+#instance
+
